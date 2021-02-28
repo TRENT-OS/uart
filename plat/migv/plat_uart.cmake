d1f1ebdab4d2ae500cf682793f6cceeca653637c
Axel Heider <axel.heider@hensoldt-cyber.de>
2021-02-28 03:17:31 +0100

    [DEV] add support for MiG-V platform

---
+++ plat/migv/plat_uart.cmake
@@ -0,0 +1,12 @@
+#
+# UART defaults for platform MiG-V
+#
+# Copyright (C) 2020-2021, HENSOLDT Cyber GmbH
+#
+
+cmake_minimum_required(VERSION 3.17)
+
+
+UART_DeclareCAmkESComponent(UART_0 0)
+UART_DeclareCAmkESComponent(UART_1 1)
+UART_DeclareCAmkESComponent(UART_2 2)
