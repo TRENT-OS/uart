98d1130da469914730101f7484bb4eba046fa630
Axel Heider <axel.heider@hensoldt-cyber.de>
2021-03-05 14:46:22 +0100

    [DEV] do CAmkESAddCPPInclude() in parent file

--- plat/sabre/plat_uart.cmake
+++ plat/sabre/plat_uart.cmake
@@ -6,9 +6,6 @@
 
 cmake_minimum_required(VERSION 3.17)
 
-
-CAmkESAddCPPInclude("${CMAKE_CURRENT_LIST_DIR}")
-
 UART_DeclareCAmkESComponent(UART_0 0)
 UART_DeclareCAmkESComponent(UART_1 1)
 UART_DeclareCAmkESComponent(UART_2 2)
--- plat/zynq7000/plat_uart.cmake
+++ plat/zynq7000/plat_uart.cmake
@@ -6,8 +6,5 @@
 
 cmake_minimum_required(VERSION 3.17)
 
-
-CAmkESAddCPPInclude("${CMAKE_CURRENT_LIST_DIR}")
-
 UART_DeclareCAmkESComponent(UART_0 0)
 UART_DeclareCAmkESComponent(UART_1 1)
