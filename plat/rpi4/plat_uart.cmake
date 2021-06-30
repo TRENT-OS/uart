7b2b3e747bf9b54e1bac03be779d1631e4e5a3b7
Lukas Graber <lukas.graber@hensoldt-cyber.de>
2021-06-30 09:17:04 +0200

    [DEV] SEOS-2341: Add rpi4 platform

---
+++ plat/rpi4/plat_uart.cmake
@@ -0,0 +1,14 @@
+#
+# UART defaults for platform rpi4 (bcm2711 based Raspberry Pi board)
+#
+# Copyright (C) 2020-2021, HENSOLDT Cyber GmbH
+#
+
+cmake_minimum_required(VERSION 3.17)
+
+UART_DeclareCAmkESComponent(UART_0 0)
+UART_DeclareCAmkESComponent(UART_1 1)
+UART_DeclareCAmkESComponent(UART_2 2)
+UART_DeclareCAmkESComponent(UART_3 3)
+UART_DeclareCAmkESComponent(UART_4 4)
+UART_DeclareCAmkESComponent(UART_5 5)
