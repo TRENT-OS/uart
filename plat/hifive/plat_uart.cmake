81309ca4db079ccba6b536ddf4e10bee4d3dc618
Axel Heider <axel.heider@hensoldt-cyber.de>
2021-07-24 21:50:59 +0200

    [DEV] SEOS-2896 add support for HiFive platform

---
+++ plat/hifive/plat_uart.cmake
@@ -0,0 +1,10 @@
+#
+# UART defaults for platform hifive (SiFive HiFive Unleashed board)
+#
+# Copyright (C) 2021, HENSOLDT Cyber GmbH
+#
+
+cmake_minimum_required(VERSION 3.17)
+
+UART_DeclareCAmkESComponent(UART_0 0)
+UART_DeclareCAmkESComponent(UART_1 1)
