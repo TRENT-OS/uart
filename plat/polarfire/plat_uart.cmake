68ff3e619067121d4449fce76aadff9bf87a0462
Axel Heider <axel.heider@hensoldt.net>
2022-04-11 18:08:40 +0200

    [DEV] add support for polarfire platform

---
+++ plat/polarfire/plat_uart.cmake
@@ -0,0 +1,12 @@
+#
+# UART defaults for Microchip Icicle Board (Polarfire SOC, SiFive U54 cores)
+#
+# Copyright (C) 2020-2021, HENSOLDT Cyber GmbH
+#
+
+cmake_minimum_required(VERSION 3.17)
+
+UART_DeclareCAmkESComponent(UART_0 0)
+UART_DeclareCAmkESComponent(UART_1 1)
+UART_DeclareCAmkESComponent(UART_2 2)
+UART_DeclareCAmkESComponent(UART_3 3)
