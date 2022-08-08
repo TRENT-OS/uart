9acb962eed4eb73735625291f95641e94b67ca7d
Axel Heider <axel.heider@hensoldt.net>
2022-08-08 20:45:19 +0200

    [DEV] stop on overflow

--- Uart.c
+++ Uart.c
@@ -38,7 +38,6 @@ typedef struct
     size_t           cnt_intr;
     size_t           cnt_data;
     size_t           cnt_data_delta;
-    size_t           cnt_fifo_full;
 #endif
 } ctx_t;
 
@@ -71,6 +70,7 @@ drain_input_fifo(
 {
     if (isOverflow(ctx))
     {
+        Debug_LOG_ERROR("isOverflow");
         return;
     }
 
@@ -84,20 +84,11 @@ drain_input_fifo(
         size_t size = FifoDataport_getContiguousFree(ctx->inputFifo, &buffer);
         if (0 == size)
         {
-
-#ifdef UART_DRIVER_PROFILING
-            ctx->cnt_fifo_full++;
-#endif
-
-            // If the interrupt is level-triggered, then giving up our time
-            // slice here can relax the situation a lot. The app gets time to
-            // drain the dataport FIFO and when the next interrupt arrives
-            // here, we should be able to drain the hardware FIFO a bit more.
-            // In QEMU, adding a yield() when the app is running at the same
-            // priority as this driver, we get 1000 bytes per interrupt drained
-            // in the throughput test, without the yield it's a poor 4 bytes
-            // per interrupt.
-            seL4_Yield();
+            /* There is no space left in the dataport FIFO, we are in an
+             * overflow condition now.
+             */
+            Debug_LOG_ERROR("dataport FIFO full");
+            setOverflow(ctx, true);
             return;
         }
 
@@ -144,10 +135,9 @@ drain_input_fifo(
         {
             ctx->cnt_data_delta = 0;
             Debug_LOG_INFO(
-                "data: %zu (0x%zx), num intr: %zu, num fifo full: %zu, bytes/intr: %zu",
+                "data: %zu (0x%zx), num intr: %zu, bytes/intr: %zu",
                 ctx->cnt_data, ctx->cnt_data,
                 ctx->cnt_intr,
-                ctx->cnt_fifo_full,
                 ctx->cnt_data / ctx->cnt_intr);
         }
 #endif
@@ -175,6 +165,13 @@ dev_irq_handle(
 #endif
         ctx.ps_cdev.handle_irq(&(ctx.ps_cdev));
         drain_input_fifo(&ctx);
+
+        if (isOverflow(&ctx))
+        {
+            /* ToDo: save interrupt and ack once overflow is cleaned */
+            Debug_LOG_ERROR("overflow detected, don't ack interrupt");
+            return;
+        }
     }
 
     int ret = dev_irq_acknowledge(irq);
