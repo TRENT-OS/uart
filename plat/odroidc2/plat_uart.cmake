257fe32e376ca39c3af4143364e6d8840d84c0d3
Axel Heider <axel.heider@hensoldt.net>
2022-08-09 21:30:40 +0200

    [DEV] add support for Odroid-C2

---
+++ plat/odroidc2/plat_uart.cmake
@@ -0,0 +1,10 @@
+#
+# UARTs defaults for platform Odroid-C2
+#
+# Copyright (C) 2022, HENSOLDT Cyber GmbH
+#
+
+cmake_minimum_required(VERSION 3.17)
+
+UART_DeclareCAmkESComponent(UART_0 3)
+UART_DeclareCAmkESComponent(UART_1 0)
