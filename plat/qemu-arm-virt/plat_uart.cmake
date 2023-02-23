a791ebd8b45dfc1106e4c36629ac64f8571aed71
Sebastian Eckl <sebastian.eckl@hensoldt.net>
2023-02-23 17:20:34 +0100

    [DEV] add support for custom qemu-arm-virt

---
+++ plat/qemu-arm-virt/plat_uart.cmake
@@ -0,0 +1,13 @@
+#
+# UARTs defaults for platform qemu-arm-virt
+#
+# Copyright (C) 2022, HENSOLDT Cyber GmbH
+#
+
+cmake_minimum_required(VERSION 3.17)
+
+UART_DeclareCAmkESComponent(UART_0 0)
+UART_DeclareCAmkESComponent(UART_1 1)
+UART_DeclareCAmkESComponent(UART_2 2)
+UART_DeclareCAmkESComponent(UART_3 3)
+UART_DeclareCAmkESComponent(UART_4 4)
