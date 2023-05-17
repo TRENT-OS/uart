c87eded6cd463a96d2004302139b0eb74c23c387
Axel Heider <axel.heider@hensoldt.net>
2023-05-17 12:04:07 +0200

    [DEV] use UART names from TRM

--- plat/odroidc2/plat_uart.cmake
+++ plat/odroidc2/plat_uart.cmake
@@ -6,5 +6,8 @@
 
 cmake_minimum_required(VERSION 3.17)
 
-UART_DeclareCAmkESComponent(UART_0 3)
-UART_DeclareCAmkESComponent(UART_1 0)
+UART_DeclareCAmkESComponent(UART_A 0)
+UART_DeclareCAmkESComponent(UART_B 1)
+UART_DeclareCAmkESComponent(UART_C 2)
+UART_DeclareCAmkESComponent(UART_AO_A 3)
+UART_DeclareCAmkESComponent(UART_AO_B 4)
